b2986405f4cb89d3e523a45a9b335173b85867bb
nattfalk <michaelnattfalk@gmail.com>
2023-09-18 22:11:22 +0200

    Add day12

---
+++ 2015/day12/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+
+var accountingBook = JObject.Parse(File.ReadAllText(@"..\..\..\input.json"));
+
+var sum = Summarize(accountingBook); 
+Console.WriteLine($"Part 1 = {sum}");
+
+var sumPart2 = Summarize(accountingBook, true);
+Console.WriteLine($"Part 2 = {sumPart2}");
+
+Console.WriteLine("Done!");
+
+static long Summarize(JToken token, bool skipRedObjects = false)
+{
+    var sum = 0L;
+    foreach (var t in token)
+    {
+        if (skipRedObjects 
+            && t.Type == JTokenType.Object 
+            && t.Values().Any(t => t.Type == JTokenType.String && t.ToString() == "red"))
+            continue;
+        
+        if (t.Type == JTokenType.Integer)
+            sum += t.Value<int>();
+        
+        sum += Summarize(t, skipRedObjects);
+    }
+    return sum;
+}
