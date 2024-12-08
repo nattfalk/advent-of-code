e527b60574e38c4d422095a9c349c2fe74e441aa
nattfalk <michaelnattfalk@gmail.com>
2024-12-08 22:12:00 +0100

    Solutions for day 6 and 7

---
+++ 2024/day06/Program.cs
@@ -0,0 +1,92 @@
+﻿var map = File.ReadAllLines("input.txt")
+    .Select(l => l.ToCharArray()).ToArray();
+
+var dir = Dir.Up;
+var (xs, ys) = GetStartPos();
+List<(int, int, Dir)> path = [];
+
+Patrol(path, xs, ys, dir);
+Console.WriteLine($"Part 1: {path.DistinctBy(x => (x.Item1, x.Item2)).Count()}");
+
+var part2 = 0;
+for (var y = 0; y < map.Length; y++)
+for (var x = 0; x < map[y].Length; x++)
+{
+    if (map[y][x] != '.')
+        continue;
+    map[y][x] = '#';
+    var tmpPath = new List<(int, int, Dir)>();
+    if (Patrol(tmpPath, xs, ys, dir))
+        part2++;
+
+    map[y][x] = '.';
+}
+
+Console.WriteLine($"Part 2: {part2}");
+return;
+
+bool Patrol(ICollection<(int, int, Dir)> path, int xs, int ys, Dir dir)
+{
+    while (true)
+    {
+        if (path.Contains((xs, ys, dir)))
+            return true;
+
+        var (x, y) = GetNextPos(xs, ys, dir);
+        if (!IsInside(x, y))
+        {
+            path.Add((xs, ys, dir));
+            break;
+        }
+
+        if (IsBlocked(x, y))
+        {
+            dir = GetNextDir(dir);
+            continue;
+        }
+
+        path.Add((xs, ys, dir));
+        xs = x;
+        ys = y;
+    }
+
+    return false;
+}
+
+bool IsInside(int x, int y) => y >= 0 && y < map.Length && x >= 0 && x < map[y].Length;
+bool IsBlocked(int x, int y) => map[y][x] == '#';
+
+(int, int) GetNextPos(int x, int y, Dir dir) =>
+    dir switch
+    {
+        Dir.Up => (x, y - 1),
+        Dir.Right => (x + 1, y),
+        Dir.Down => (x, y + 1),
+        Dir.Left => (x - 1, y),
+    };
+
+Dir GetNextDir(Dir dir) =>
+    dir switch
+    {
+        Dir.Up => Dir.Right,
+        Dir.Right => Dir.Down,
+        Dir.Down => Dir.Left,
+        Dir.Left => Dir.Up,
+    };
+
+(int, int) GetStartPos()
+{
+    for (var y = 0; y < map.Length; y++)
+    for (var x = 0; x < map[y].Length; x++)
+        if (map[y][x] == '^')
+            return (x, y);
+    return (-1, -1);
+}
+
+internal enum Dir
+{
+    Up,
+    Right,
+    Down,
+    Left
+}
---
+++ 2024/day07/Program.cs
@@ -0,0 +1,79 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var data = new List<Input>();
+
+foreach (var line in input)
+{
+    var parts = line.Split(": ");
+    var sum = long.Parse(parts[0]);
+    var numbers = parts[1].Split(" ").Select(long.Parse).ToArray();
+    data.Add(new Input(sum, numbers));
+}
+
+long part1 = 0;
+long part2 = 0;
+foreach (var entry in data)
+{
+    var result = CalculateSum(entry.Numbers, entry.Sum);
+    part1 += result;
+    if (result == 0)
+    {
+        result = CalculateSum(entry.Numbers, entry.Sum, true);
+        if (result == entry.Sum)
+        {
+            part2 += result;
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part1 + part2}");
+
+return;
+
+long CalculateSum(long[] numbers, long sum, bool useOr = false)
+{
+    List<Op> ops = useOr ? [Op.Add, Op.Mul, Op.Or] : [Op.Add, Op.Mul];
+    var combinations = GetCombinations(ops, numbers.Length - 1);
+    foreach (var comb in combinations)
+    {
+        var result = numbers[0];
+        var i = 1;
+        foreach (var op in comb)
+        {
+            result = op switch
+            {
+                Op.Add => result + numbers[i],
+                Op.Mul => result * numbers[i],
+                Op.Or => long.Parse($"{result}{numbers[i]}"),
+                _ => result
+            };
+            i++;
+        }
+
+        if (result == sum)
+        {
+            return result;
+        }
+    }
+
+    return 0;
+}
+
+IEnumerable<IEnumerable<T>> GetCombinations<T>(IEnumerable<T> list, int length)
+{
+    if (length == 1) return list.Select(t => new T[] { t });
+
+    return GetCombinations(list, length - 1)
+        .SelectMany(t => list, (t1, t2) => t1.Concat(new T[] { t2 }));
+}
+
+
+enum Op
+{
+    Add,
+    Mul,
+    Or
+}
+
+record Input(long Sum, long[] Numbers);
